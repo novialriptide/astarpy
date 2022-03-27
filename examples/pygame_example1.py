ff5c8655446ca4004faaf59dc596b53eb0cec281
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-03-27 12:44:25 -0400

    made a more interactive example experience

--- examples/pygame_example1.py
+++ examples/pygame_example1.py
@@ -3,49 +3,71 @@ import sys
 import astarpy as pf
 
 g = pf.Graph(20, 20)
-g.set_barrier(pygame.Vector2(4, 2))
-g.set_barrier(pygame.Vector2(3, 2))
-g.set_barrier(pygame.Vector2(2, 2))
-g.set_barrier(pygame.Vector2(1, 2))
-g.set_barrier(pygame.Vector2(12, 8))
-g.set_barrier(pygame.Vector2(12, 9))
-g.set_barrier(pygame.Vector2(12, 10))
-g.set_barrier(pygame.Vector2(12, 11))
-g.set_barrier(pygame.Vector2(12, 12))
-g.set_barrier(pygame.Vector2(12, 13))
-g.set_barrier(pygame.Vector2(12, 14))
-g.set_barrier(pygame.Vector2(12, 15))
+g.set_barrier((9, 15))
+g.set_barrier((10, 15))
+g.set_barrier((11, 15))
+g.set_barrier((12, 15))
+g.set_barrier((13, 15))
+g.set_barrier((14, 15))
+g.set_barrier((15, 15))
 
 pygame.init()
 screen = pygame.display.set_mode((640, 640))
+clock = pygame.time.Clock()
 
-block_pos = pygame.Vector2(2, 1)
-path = g.get_path(pygame.Vector2(2, 1), pygame.Vector2(15, 17))
+block_pos = (2, 1)
 block_length = 25
+
+rects = []
+
+for n in g.nodes:
+    rects.append(
+        {
+            "rect": pygame.Rect(
+                n.pos[0] * block_length,
+                n.pos[1] * block_length,
+                block_length,
+                block_length,
+            ),
+            "color": (255, 255, 255),
+            "node": n,
+        }
+    )
+
+path = g.get_path((2, 1), (15, 17))
+next_path = 0
+ms_to_next_path = 1000
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    for n in g.nodes:
-        if n.barrier:
+    if pygame.time.get_ticks() > next_path:
+        start = pygame.time.get_ticks()
+        path = g.get_path((2, 1), (15, 17))
+        end = pygame.time.get_ticks()
+        print(f"it took {(end - start) / 1000}ms to find a path")
+        next_path = pygame.time.get_ticks() + ms_to_next_path
+
+    for r in rects:
+        if r["node"].barrier:
             color = (100, 0, 0)
         else:
             color = (255, 255, 255)
 
-        if n in path:
+        if r["node"] in path:
             color = (0, 255, 0)
 
         pygame.draw.rect(
             screen,
             color,
-            (
-                n.pos.x * block_length,
-                n.pos.y * block_length,
-                block_length,
-                block_length,
-            ),
+            r["rect"],
         )
 
+        if r["rect"].collidepoint(pygame.mouse.get_pos()):
+            g.set_barrier(r["node"].pos)
+
     pygame.display.update()
+    pygame.display.set_caption(f"astarpy (fps: {clock.get_fps()})")
+    clock.tick(0)
